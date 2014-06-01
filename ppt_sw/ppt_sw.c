df6b0da7256fa2e06318c74183c2ce5b14c6a622
gnrr <hideaki.sh@gmail.com>
2014-06-02 00:09:49 +0900

    - untabified - determin sw_type by pulling it while startup

--- ppt_sw/ppt_sw.c
+++ ppt_sw/ppt_sw.c
@@ -3,7 +3,7 @@
  *
  * Created: 11/6/2013 2:28:04 PM
  *  Author: g
- */ 
+ */
 
 #include <avr/io.h>
 #include "mystd.h"
@@ -20,95 +20,115 @@ u1 report[] = {0xFD, 0x09, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}
 
 #define SW_TYPE_A     0             // normal open
 #define SW_TYPE_B     1             // normal close
-u1 sw_type_back;                    // SW_TYPE_A: BERETTA
+u1 sw_type_fw, sw_type_bk;          // SW_TYPE_A: BERETTA
                                     // SW_TYPE_B: SIG, GLOCK
+
+#define IS_CLOSED_SW_FORWARD()    (!(PINB & (1<<PINB0))) 
+#define IS_CLOSED_SW_BACKWARD()   (!(PINB & (1<<PINB1))) 
 
 bool is_pressed_forward(void)
 {
     u1 *pks = &keystate[KS_FORWARD];
-    u1 k = (PINB & (1<<PINB0))? FALSE:TRUE;
+    u1 k = IS_CLOSED_SW_FORWARD();
+
+    if(sw_type_fw == SW_TYPE_A) k = !k;
+
     if((k == TRUE) && (*pks == 0))
-		*pks = 1;
+        *pks = 1;
     else if((k == FALSE) && (*pks == 1))
-		*pks = 2;
+        *pks = 2;
 
     return (*pks == 2);
 }
 
 bool is_pressed_backward(void)
 {
-	u1 *pks = &keystate[KS_BACKWARD];
-    u1 k;
+    u1 *pks = &keystate[KS_BACKWARD];
+    u1 k = IS_CLOSED_SW_BACKWARD();
 
-    if(sw_type_back == SW_TYPE_A)
-        k = (PINB & (1<<PINB1))? FALSE:TRUE;
-    else
-        k = (PINB & (1<<PINB1))? TRUE:FALSE;
+    if(sw_type_bk == SW_TYPE_A) k = !k;
 
-	if((k == TRUE) && (*pks == 0))
-		*pks = 1;
-	else if((k == FALSE) && (*pks == 1))
-		*pks = 2;
+    if((k == TRUE) && (*pks == 0))
+        *pks = 1;
+    else if((k == FALSE) && (*pks == 1))
+        *pks = 2;
 
-	return (*pks == 2);
+    return (*pks == 2);
 }
 
 void send_byte(u1 ch)
 {
-	while (!(UCSRA & (1<<UDRE)))	// Wait for empty transmit buffer
-		;
+    while (!(UCSRA & (1<<UDRE)))    // Wait for empty transmit buffer
+        ;
 
-	UDR = ch;						// Put data into buffer
+    UDR = ch;                       // Put data into buffer
 }
 
 void send_report(u1 scan_code)
 {
-	report[5] = scan_code;
-	
-	for(u1 i=0; i<sizeof(report)/sizeof(u1); i++)
+    report[5] = scan_code;
+
+    for(u1 i=0; i<sizeof(report)/sizeof(u1); i++)
         send_byte(report[i]);
 }
 
 void init(void)
 {
-	// sw
-	PORTB = (1<<PB1)|(1<<PB0);		// pull up
-    PORTD = (1<<PB5);				// pull up
-	
-	// uart
-	UCSRA |= 1<<U2X;
-	UBRRH = (u1)(BPS>>8);
-	UBRRL = (u1)BPS;
-	UCSRB |= 1<<TXEN;				// Enable transmitter
-	
-	// vars
-                                                                 //                            open     short
-    sw_type_back = (PIND & (1<<PIND5))? SW_TYPE_B:SW_TYPE_A;     // PD5(PIN#9) -- GND(PIN#10)  TYPE_B   TYPE_A
+    // sw
+    PORTB = (1<<PB1)|(1<<PB0);      // pull up
+    PORTD = (1<<PB5);               // pull up
+
+    // uart
+    UCSRA |= 1<<U2X;
+    UBRRH = (u1)(BPS>>8);
+    UBRRL = (u1)BPS;
+    UCSRB |= 1<<TXEN;               // Enable transmitter
+
+    // vars
+}
+
+void set_sw_type(void)
+{
+    const u2 MAX = 3000;
+    const u2 THRESHOLD = 1000;
+    u2 i;
+    u1 *pfw = &sw_type_fw;
+    u1 *pbk = &sw_type_bk;
+    u2 cnt_fw, cnt_bk;
+
+    for(i=0; i<MAX; i++) {
+        cnt_fw = (PINB & (1<<PINB0))? 0 : cnt_fw+1;  // pull: ++ 
+        cnt_bk = (PINB & (1<<PINB1))? 0 : cnt_bk+1;
+    }
+
+    *pfw = (cnt_fw < THRESHOLD)? SW_TYPE_A : SW_TYPE_B;
+    *pbk = (cnt_bk < THRESHOLD)? SW_TYPE_A : SW_TYPE_B;
 }
 
 int main(void)
 {
-	bool usb_send_press = FALSE;
+    bool usb_send_press = FALSE;
 
-	init();
-	
+    init();
+    set_sw_type();
+
     while(1)
     {
-		if(usb_send_press == FALSE) {
-			if(is_pressed_forward()) {
-				send_report(USID_KBD_PAGE_DOWN);
-				usb_send_press = TRUE;
-			} else if(is_pressed_backward()) {
-				send_report(USID_KBD_PAGE_UP);
-				usb_send_press = TRUE;
-			}
-		} else {
-			send_report(0x00);      // released
-			usb_send_press = FALSE;
+        if(usb_send_press == FALSE) {
+            if(is_pressed_forward()) {
+                send_report(USID_KBD_PAGE_DOWN);
+                usb_send_press = TRUE;
+            } else if(is_pressed_backward()) {
+                send_report(USID_KBD_PAGE_UP);
+                usb_send_press = TRUE;
+            }
+        } else {
+            send_report(0x00);      // released
+            usb_send_press = FALSE;
 
-			for(u1 i=0;i<sizeof(keystate); i++)
-				keystate[i] = 0;
-		}
+            for(u1 i=0;i<sizeof(keystate); i++)
+                keystate[i] = 0;
+        }
     }
-	return EXIT_FAILURE;
+    return EXIT_FAILURE;
 }
